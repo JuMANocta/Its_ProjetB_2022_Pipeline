a0717e85d69e345ef9a9bce3c6fdfed083d4f95c
JuMANocta <93329464+JuMANocta@users.noreply.github.com>
2024-10-09 15:11:23 +0200

    Delete src/main/java/com/compagny/myapp/PersonneRepository.java

--- src/main/java/com/compagny/myapp/PersonneRepository.java
+++
@@ -1,7 +0,0 @@
-package com.compagny.myapp;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonneRepository extends JpaRepository<Personne, Long> {}
